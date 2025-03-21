80b970301b62b4f31b4526f33eb948b8d1a7c5f1
Povilas <paulsirut@gmail.com>
2025-03-21 11:46:02 +0100

    add grid, move header

--- script/main.js
+++ script/main.js
@@ -10,13 +10,11 @@ document.getElementById("opretKnap").onclick = function() {
     modalOpret.style.display = 'block';
 };
 
-// Close modal when clicking outside of it
+// Close modals when clicking outside of them
 window.onclick = function(event) {
     if (event.target === modalLogin) {
         modalLogin.style.display = "none";
     }
-};
-window.onclick = function(event) {
     if (event.target === modalOpret) {
         modalOpret.style.display = "none";
     }
