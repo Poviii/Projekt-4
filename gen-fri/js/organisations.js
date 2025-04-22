4cf89dd351ab7c7c93ac872d921c15b8366b8595
Povilas <paulsirut@gmail.com>
2025-04-22 11:02:10 +0200

    fix

--- gen-fri/js/organisations.js
+++ gen-fri/js/organisations.js
@@ -13,7 +13,7 @@ for (var i = 0; i < likeKnapper.length; i++) {
     // Event
     knap.addEventListener("click", function() {
         // If-else
-        if (this.src.includes("/gen-fri/icons/orange_icons/heart_orange.svg")) {
+        if (this.src.includes("/gen-fri/icons/black_icons/heart_black.svg")) {
             this.src = "/gen-fri/icons/orange_icons/heart_orange.svg";
             alert("Du har tilføjet en organisation!");
         } else {
